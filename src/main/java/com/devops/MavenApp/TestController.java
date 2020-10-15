709ca710ff949018a5a225c927400f377ff2bb76
vimlesh88d <kumarvimlesh.it@gmail.com>
2020-10-15 19:10:46 +0530

    Added new Project

---
+++ src/main/java/com/devops/MavenApp/TestController.java
@@ -0,0 +1,13 @@
+package com.devops.MavenApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Welcome to India";
+	}
+
+}
